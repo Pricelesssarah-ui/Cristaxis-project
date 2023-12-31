60adf6639b648422a5cf3f26152d2a9050ca2444
Pricelesssarah-ui <pricelesssarah3@gmail.com>
2023-12-31 15:57:38 +0100

    final changes

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -60,15 +60,15 @@ function Booking() {
         email: values?.email,
         sameDayBooking: selectedOption === "sameDayBooking" ? true : false,
         travelNotes: values?.travelNotes,
-        //termsAndConditions: values?.termsAndConditions,
+        t_c: true,
       };
 
       if (selectedOption === "later") {
         selectedDate.year = Number(selectedDate.year);
-        selectedDate.month = Number(selectedDate.month);
-        selectedDate.day = Number(selectedDate.day);
+        selectedDate.month = String(selectedDate.month);
+        selectedDate.day = String(selectedDate.day);
         selectedDate.hour = Number(selectedDate.hour);
-        selectedDate.minute = Number(selectedDate.minute);
+        selectedDate.minute = String(selectedDate.minute);
         selectedDate.meridian = selectedDate.meridian || "AM";
 
         submittedValues.scheduledRide = selectedDate;
@@ -86,9 +86,6 @@ function Booking() {
       console.error("Book a taxi Error", error);
     }
   };
-
-  const options = Array.from({ length: 31 }, (_, index) => index + 1);
-  const minutes = Array.from({ length: 60 }, (_, index) => index);
 
   return (
     <motion.div
@@ -303,15 +300,15 @@ function Booking() {
                     className="bookinginputField tripInput py-3 px-3 my-3 mx-0 w-[80%]"
                   >
                     <option value="">Month</option>
-                    <option value="1">January</option>
-                    <option value="2">February</option>
-                    <option value="3">March</option>
-                    <option value="4">April</option>
-                    <option value="5">May</option>
-                    <option value="6">June</option>
-                    <option value="7">July</option>
-                    <option value="8">August</option>
-                    <option value="9">September</option>
+                    <option value="01">January</option>
+                    <option value="02">February</option>
+                    <option value="03">March</option>
+                    <option value="04">April</option>
+                    <option value="05">May</option>
+                    <option value="06">June</option>
+                    <option value="07">July</option>
+                    <option value="08">August</option>
+                    <option value="09">September</option>
                     <option value="10">October</option>
                     <option value="11">November</option>
                     <option value="12">December</option>
@@ -322,12 +319,38 @@ function Booking() {
                     onChange={handleDateChange}
                     className="bookinginputField tripInput py-3 my-3 mx-0 w-[80%]"
                   >
-                    <option value="">Day</option>
-                    {options.map((value) => (
-                      <option key={value} value={value}>
-                        {value}
-                      </option>
-                    ))}
+                   <option value="">Day</option>
+                    <option value="01">01</option>
+                    <option value="02">02</option>
+                    <option value="03">03</option>
+                    <option value="04">04</option>
+                    <option value="05">05</option>
+                    <option value="06">06</option>
+                    <option value="07">07</option>
+                    <option value="08">08</option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
                   </select>
 
                   <select
@@ -355,12 +378,67 @@ function Booking() {
                     onChange={handleTimeChange}
                     className="bookinginputField tripInput py-3 my-3 mx-0 w-[80%]"
                   >
-                    <option value="">Minutes</option>
-                    {minutes.map((minute) => (
-                      <option key={minute} value={minute}>
-                        {minute < 10 ? `0${minute}` : minute}
-                      </option>
-                    ))}
+                    <option value="">Minute</option>
+                    <option value="00">00</option>
+                    <option value="01">01</option>
+                    <option value="02">02</option>
+                    <option value="03">03</option>
+                    <option value="04">04</option>
+                    <option value="05">05</option>
+                    <option value="06">06</option>
+                    <option value="07">07</option>
+                    <option value="08">08</option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+                    <option value="32">32</option>
+                    <option value="33">33</option>
+                    <option value="34">34</option>
+                    <option value="35">35</option>
+                    <option value="36">36</option>
+                    <option value="37">37</option>
+                    <option value="38">38</option>
+                    <option value="39">39</option>
+                    <option value="40">40</option>
+                    <option value="41">41</option>
+                    <option value="42">42</option>
+                    <option value="43">43</option>
+                    <option value="44">44</option>
+                    <option value="45">45</option>
+                    <option value="46">46</option>
+                    <option value="47">47</option>
+                    <option value="48">48</option>
+                    <option value="49">49</option>
+                    <option value="50">50</option>
+                    <option value="51">51</option>
+                    <option value="52">52</option>
+                    <option value="53">53</option>
+                    <option value="54">54</option>
+                    <option value="55">55</option>
+                    <option value="56">56</option>
+                    <option value="57">57</option>
+                    <option value="58">58</option>
+                    <option value="59">59</option>
                   </select>
 
                   <select
